5356f434221f644a9476b575012948f45aeab3cf
kyaanise1 <klydelmaeasino@gmail.com>
2026-09-10 11:47:49 +0800

    Add behavior monitoring project files

---
+++ extract_frames.py
@@ -0,0 +1,57 @@
+import cv2
+import os
+
+# Input video
+video_path = r"C:\Acads\4th Year\1st Semester\Thesis\Behavior Monitoring\video\Day_01_2026-08-27_0600_to_0700.mkv"
+
+# Folder where extracted images will be saved
+output_folder = "extracted_frames"
+
+os.makedirs(output_folder, exist_ok=True)
+
+cap = cv2.VideoCapture(video_path)
+
+fps = cap.get(cv2.CAP_PROP_FPS)
+total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+duration = total_frames / fps
+
+print("FPS:", fps)
+print("Total frames:", total_frames)
+print("Duration:", duration / 3600, "hours")
+
+interval = 5
+
+current_time = 0
+image_count = 0
+
+while current_time < duration:
+
+    # Move video to the desired time
+    cap.set(cv2.CAP_PROP_POS_MSEC, current_time * 1000)
+
+    # Read the frame
+    ret, frame = cap.read()
+
+    if not ret:
+        break
+
+    # Save the frame
+    filename = os.path.join(
+        output_folder,
+        f"frame_{image_count:05d}.jpg"
+    )
+
+    cv2.imwrite(filename, frame)
+
+    image_count += 1
+
+    print(f"Saved: {filename} | Time: {current_time:.1f} seconds")
+
+    # Move forward 5 seconds
+    current_time += interval
+
+# Release video
+cap.release()
+
+print("\nExtraction complete!")
+print("Total images:", image_count)
