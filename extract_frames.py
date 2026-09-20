1df6023f0048884909ca0e711e73ace08df3cf28
kyaanise1 <klydelmaeasino@gmail.com>
2026-09-21 00:11:07 +0800

    feat: add labels day 5 1-400

--- extract_frames.py
+++ extract_frames.py
@@ -2,7 +2,7 @@ import cv2
 import os
 
 # Input video
-video_path = r"C:\Acads\4th Year\1st Semester\Thesis\Behavior Monitoring\video\Day_03_2026-08-29_0800_to_0900.mkv"
+video_path = r"C:\Acads\4th Year\1st Semester\Thesis\Behavior Monitoring\video\Day_05_2026-08-31_1000_to_1100.mkv"
 
 # Folder where extracted images will be saved
 output_folder = "extracted_frames"
@@ -29,7 +29,7 @@ print("Duration:", duration / 3600, "hours")
 interval = 5
 
 # Day number
-day_number = 3  # Change this to the appropriate day number
+day_number = 5  # Change this to the appropriate day number
 
 # Starting frame number for this day
 start_number = day_number * 1000
